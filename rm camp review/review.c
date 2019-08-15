b359df81bafee7ba9b949b32cf7f42f322524c19
character76 <39854767+character76@users.noreply.github.com>
2019-08-15 19:41:04 +0800

    Update review.c

--- rm camp review/review.c
+++ rm camp review/review.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #define True 1
 #define False 0
+#define Error
 //basic review of binary search and quicksort
 int main()
 {
@@ -21,23 +22,43 @@ int search(int *arr,int len,int key)
     if(len>1)
     {
         if(arr[middle]<key)
-        search_help(arr,&arr[middle],key);
+        return search_help(arr,&arr[middle],key);
         if(arr[middle]>key)
-        search_help(&arr[middle],&arr[len-1],key);
+        return search_help(&arr[middle],&arr[len-1],key);
     }
 }
 int search_help(int* left,int* right,int key)
 {
-    if(right-left==1)
+    int len=(right-left)/sizeof(int)+1;
+    
+    printf("len:%d",len);
+    if(right-left==sizeof(int))
     {
-        if(arr[right]==key||arr[left]==key)
+        if(*left==key||*right==key)
         {
-        return True;
+            return True;
         }
         else return False;
     }
     else if(right-left==0)
     {
-        
+
+    }
+    else if(right-left>sizeof(int))
+    {
+        int* middle =left+(len/2)*sizeof(int);
+        if(*middle==key)
+        {
+            return True;
+        }
+        else if(key<*middle)
+        {
+            return search_help(left,middle,key);    
+        }
+        else if(key>*middle)
+        {
+            return search_help(middle,right,key);
+        }
+    
     }
 }
